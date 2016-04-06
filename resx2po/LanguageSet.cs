f426829d288ed7eea8b9ca3fda83f54f6a286496
Patrick Simpson <p.simpson@etosis.com>
2016-04-06 17:46:46 +0200

    Removed obsolete files

---
+++ resx2po/LanguageSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etosis.resx2po
+{
+    /// <summary>
+    /// Combined multiple resources files that represent the same strings
+    /// in multiple languages.
+    /// </summary>
+    public class ResourceSet
+    {
+    }
+}
